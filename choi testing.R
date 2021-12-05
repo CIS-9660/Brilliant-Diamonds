0175206e1d3b2f88403053e26ae00705c133b057
Vanessa Choi <vivin7654@gmail.com>
2021-12-04 21:59:26 -0500

    sadsadsad

--- choi testing.R
+++ choi testing.R
@@ -1,4 +1,4 @@
 # hi this is test 
 
-git config --global user.email "vivin@gmail.com"
-git config --global user.name "Choi"
+#git config --global user.email "vivin@gmail.com"
+#git config --global user.name "Choi"
