52095d89099a1cf01a7a2228cfbd9144972ef8c5
HenryGwak <95325374+HenryGwak@users.noreply.github.com>
2021-12-08 20:43:05 -0500

    hi

--- Tree Method.R
+++ Tree Method.R
@@ -23,11 +23,35 @@ plot(tree.diamond)
 text(tree.diamond,pretty=0)
 
 # tree including selected variables
-tree.diamond=tree(price~shape+carat+cut+color+clarity+report+type,Diamond,subset=train)
-summary(tree.diamond)
-plot(tree.diamond)
-text(tree.diamond,pretty=0)
+set.seed(1)
+tree.diamond1=tree(price~shape+carat+cut+color+clarity+report+type,Diamond,subset=train)
+summary(tree.diamond1)
+plot(tree.diamond1)
+text(tree.diamond1,pretty=0)
 #Both trees produce the same result, variables actually used in tree construction are
 # "carat"   "report"  "clarity" "cut" 
+
+
+cv.diamond=cv.tree(tree.diamond)
+cv.diamond
+plot(cv.diamond$size,cv.diamond$dev,type='b')
+
+prune.diamond=prune.tree(tree.diamond,best=10)
+plot(prune.diamond)
+text(prune.diamond,pretty=0)
+summary(prune.diamond)
+
+
+yhat=predict(tree.diamond,newdata=Diamond[-train,])
+
+
+
+# true value of DV on the test data
+diamond.test=Diamond[-train,"price"]
+plot(yhat,diamond.test)
+abline(0,1)
+#MSE (Mean of Squared Errors)
+mean((yhat-diamond.test)^2)
+
 
 
